288249d67940134e1aa599e302c8fb4b9d07b4c4
Michel Marinho <michel.marinho04@outlook.com>
2022-08-10 16:29:40 -0400

    Add: Login Form

---
+++ src/app/autenticacao/autenticacao.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AutenticacaoService {
+  constructor(private httpClient: HttpClient) {}
+  autenticar(usuario: String, senha: string): Observable<any> {
+    return this.httpClient.post('http://localhost:3000/user/login', {
+      userName: usuario,
+      password: senha,
+    });
+  }
+}
